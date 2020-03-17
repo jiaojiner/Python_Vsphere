93231d0f148578f0b92c03cd4bbeebeb030265dc
jiaojiner <15148365776@163.com>
2020-03-17 19:30:10 +0800

    vshere api

---
+++ API/cred.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- encoding = utf-8 -*-
+# 该代码由本人学习时编写，仅供自娱自乐！
+# 本人QQ：1945962391 
+# 欢迎留言讨论，共同学习进步！
+
+vcip = '172.16.1.200'
+username = 'administrator@vshere.local'
+password = 'Cisc0123'
